af7f92c133f977af5e316131e9b4869e14ca1f70
mapeng <mapeng@baidu.com>
2021-12-01 22:00:24 +0800

    双指针

--- TwoPointer.py
+++ TwoPointer.py
@@ -13,7 +13,26 @@ class Solution(object):
                 res = [i, k]
                 break
         return res
+    
+    def sortColors(self, nums: list) -> list:
+        n = len(nums)
+        p1 = 0
+        p2 = n-1
+        i = 0
+        while i <= p2:
+            while i <= p2 and nums[i] == 2:
+                nums[i], nums[p2] = nums[p2], nums[i]
+                p2 -= 1
+            if nums[i] == 0:
+                nums[i], nums[p1] = nums[p1], nums[i]
+                p1 += 1
+            i += 1
+        return nums
+
+
+
 
 so = Solution()
-res = so.twoSum2([2,7,22,23], 9)
+# res = so.twoSum2([2,7,22,23], 9)
+res = so.sortColors([0,1,2,1,2,0,1,2,0])
 print(res)
